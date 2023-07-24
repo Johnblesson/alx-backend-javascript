cd671c51f4761c2b60748711951571161f1b36b2
John Blesson-Rowe <106766947+Johnblesson@users.noreply.github.com>
2023-07-24 12:36:49 +0000

    update files

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,5 +1,4 @@
-/* function taskFirst to instantiate variables using const */
-/* function taskNext to instantiate variables using let */
+/* Given what you’ve read about var and hoisting, modify the variables inside the function taskBlock so that the variables aren’t overwritten inside the conditional block. */
 export default function taskBlock(trueOrFalse) {
   var task = false;
   var task2 = true;
