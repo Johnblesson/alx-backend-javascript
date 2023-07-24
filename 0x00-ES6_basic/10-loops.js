bd24ee5f81f92be8ac27bcdbbd84bdfe1658d0e8
Johnblesson <johnblessonrowe@gmail.com>
2023-07-24 11:38:15 +0000

    update

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,11 @@
-/* eslint-disable guard-for-in */
-/* eslint-disable no-param-reassign */
 export default function appendToEachArrayValue(array, appendString) {
   for (let value of array) {
     value = appendString + value;
   }
 
   return array;
+}
+
+export default function appendToEachArrayValue(array, appendString) {
+  return array.map(value => appendString + value);
 }
