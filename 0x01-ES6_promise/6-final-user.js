22ce3b80cfb9fe8b1bfa7ed1156338bf8a879ecc
John Blesson-Rowe <106766947+Johnblesson@users.noreply.github.com>
2023-07-26 08:58:40 +0000

    update final user

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,23 +1,15 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import { signUpUser } from './4-user-promise';
+import { uploadPhoto } from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [signUpUser(firstName, lastName),
-    uploadPhoto(fileName)];
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const user = await signUpUser(firstName, lastName).then((data) => ({
+    status: 'fulfilled',
+    value: data,
+  }));
 
-  return Promise.allSettled(promises)
-    .then((values) => {
-      const res = [];
-      values.forEach((value) => {
-        if (value.status === 'fulfilled') {
-          res.push(value);
-        } else {
-          res.push({
-            status: value.status,
-            value: `Error: ${value.reason.message}`,
-          });
-        }
-      });
-      return res;
-    });
+  const photUpload = await uploadPhoto(fileName).catch((error) => ({
+    status: 'rejected',
+    value: error.toString(),
+  }));
+  return Promise.resolve([user, photUpload]);
 }
